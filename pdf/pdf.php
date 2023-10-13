6c977596a56d052f4fea1728712013b6e16f6879
Akireiss <joshcstnd@gmail.com>
2023-10-13 09:00:12 +0800

    Pdf updated

--- pdf/pdf.php
+++ pdf/pdf.php
@@ -1,133 +1,165 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Pdf</title>
-
-    
-    <style type="text/css">
-        * {
-            font-family: Verdana, Arial, sans-serif;
-        }
-
-        .page-break {
-            page-break-after: always;
-        }
-
-        table {
-            font-size: x-small;
-            border-collapse: collapse;
-
-        }
-
-        table td {
-            /* border: 1px solid black; */
-            padding: 5px;
-        }
-
-        table th {
-            /* border: 1px solid black; */
-
-        }
-
-        .gray {
-            background-color: lightgray
-        }
-
-        .bold {
-            font-weight: bold;
-            padding: 8px;
-        }
-
-        .checkbox {
-            width: 12px;
-            height: 12px;
-            border: 1px solid #000;
-            /* background-color: #000; */
-            display: inline-block;
-            /* Display the checkbox inline */
-            margin-right: 4px;
-            /* Add some spacing between checkbox and label */
-        }
-
-        .check {
-            width: 12px;
-            height: 12px;
-            border: 1px solid #000;
-            background-color: #000;
-            display: inline-block;
-            /* Display the checkbox inline */
-            margin-right: 4px;
-            /* Add some spacing between checkbox and label */
-        }
-
-        .label {
-            display: inline-block;
-        }
-
-        .center {
-            text-align: center;
-            /* Center align the content within all <td> elements */
-        }
-    </style>
-</head>
 
 <body>
+    <!doctype html>
+    <html lang="en">
 
-    <table width="100%">
-        <tbody>
-                <td class="center bold">SERICULTURE RESEARCH DEVELOPMENT INSTITUTE <br/>
-                 FARMER COOPERATOR'S INFORMATION <br/
+    <head>
+        <meta charset="UTF-8">
+        <title>Pdf</title>
+
+
+        <style type="text/css">
+            * {
+                font-family: Verdana, Arial, sans-serif;
+            }
+
+            .page-break {
+                page-break-after: always;
+            }
+
+            table {
+                font-size: x-small;
+                border-collapse: collapse;
+
+            }
+
+            table td {
+                /* border: 1px solid black; */
+                padding: 5px;
+            }
+
+            table th {
+                /* border: 1px solid black; */
+
+            }
+
+            .gray {
+                background-color: lightgray
+            }
+
+            .bold {
+                font-weight: bold;
+                padding: 8px;
+            }
+
+            .checkbox {
+                width: 12px;
+                height: 12px;
+                border: 1px solid #000;
+                /* background-color: #000; */
+                display: inline-block;
+                /* Display the checkbox inline */
+                margin-right: 4px;
+                /* Add some spacing between checkbox and label */
+            }
+
+            .check {
+                width: 12px;
+                height: 12px;
+                border: 1px solid #000;
+                background-color: #000;
+                display: inline-block;
+                /* Display the checkbox inline */
+                margin-right: 4px;
+                /* Add some spacing between checkbox and label */
+            }
+
+            .label {
+                display: inline-block;
+            }
+
+            .center {
+                text-align: center;
+                /* Center align the content within all <td> elements */
+            }
+
+            .underline {
+                text-decoration: underline;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <table width="100%">
+            <tbody>
+                <td class="center bold">SERICULTURE RESEARCH DEVELOPMENT INSTITUTE <br />
+                    FARMER COOPERATOR'S INFORMATION <br />
                     and SITE INSPECTON SHEET </td>
 
 
-        </tbody>
-    </table>
+            </tbody>
+        </table>
 
-    <br/>
-    <table width="100%">
-        <tbody>
-            <tr>
+        <br />
+        <table width="100%">
+            <tbody>
+                <tr>
 
-                <td>Date of Validation:</td>
+                    <td>Date of Validation:</td>
                 </tr>
                 <tr>
 
                     <td>Personal Information:</td>
                 </tr>
-        </tbody>
-    </table>
+            </tbody>
+        </table>
 
-    <table width="100%">
-        <tbody>
-            <tr>
-                <td width="55%">Name:</td>
+        <table width="100%">
+            <tbody>
+                <tr>
+                    <td width="55%">Name: <span class="underline">Akira</span></td>
                     <td width="25%">Age:</td>
                     <td width="20%">Sex:</td>
                 </tr>
                 <tr>
                     <td width="100%">Permanent Address</td>
                 </tr>
-        </tbody>
-    </table>
+            </tbody>
+        </table>
 
-    <table width="100%">
-        <tbody>
-            <tr>
-                <td width="5%"></td>
-                <td width="50%">Name:</td>
-                <td width="25%">Religion:</td>
+        <table width="100%">
+            <tbody>
+                <tr>
+                    <td width="5%"></td>
+                    <td width="50%">Name:</td>
+                    <td width="25%">Religion:</td>
                 </tr>
-        </tbody>
-    </table>
-</body>
-</html>
+
+
+            </tbody>
+        </table>
+        <table width="100%">
+            <tbody>
+                <tr>
+                    <td width="6%"></td>
+                    <td width="25%">Civil Status</td>
+                    <td width="70%">If married, name of spouse:</td>
+                </tr>
+
+
+            </tbody>
+        </table>
+
+        <table width="100%">
+            <tbody>
+                <tr>
+                    <td widt="7%"></td>
+                    <td width="94%">
+                        Number of family members (except you)
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+    </body>
+
+    </html>
