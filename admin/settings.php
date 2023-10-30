70e8f43c1dd577ee11ce19e5cc20a0b9bdd4f231
Akireiss <joshcstnd@gmail.com>
2023-10-30 18:19:57 +0800

    fdsfsdsada

--- admin/settings.php
+++ admin/settings.php
@@ -24,115 +24,124 @@ if(!isset($_SESSION['user_id'])) {
 
 <div>
     <div>
-      <div class="row hidden-md-up">
-
-
-        <div class="col-md-4 ">
-          <div class="card pb-5 ">
-            <div class="card-block ">
-              <div class="card-title bg-success px-2">
-                Location
-              </div>
-              <div class="mx-2">
-
-                <li class="card-text my-1">
-                <a href="province.php">
-                  Province
-                </a>
-                </li>
-                <li class="card-text my-1">
-                      <a href="municipality.php">
-                Municipality
-                      </a>
-                </li>
-                <li class="card-text my-1">
-                <a href="barangay.php">Barangay</a></li>
-              </div>
-             
-            </div>
-          </div>
-        </div>
-
-
-        <div class="col-md-4">
-          <div class="card pb-5">
-            <div class="card-block">
-            <div class="card-title bg-success px-2">
-          Producers
-          </div>
-              <div class="mx-2">
-              
-              <p class="card-text p-y-1">Some quick example text to build on the card title .</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div class="col-md-4">
-          <div class="card pb-5">
-            <div class="card-block">
-            <div class="card-title bg-success px-2">
-          Production
-          </div>
-              <div class="mx-2">
-              <p class="card-text p-y-1">Some quick example text to build on the card title .</p>
-             
-            </div>
-          </div>
-        </div>
-
-
-      </div><br>
-      <div class="row">
-
-
-        <div class="col-md-4">
-          <div class="card pb-5">
-            <div class="card-block">
-            <div class="card-title bg-success px-2">
-          News
-          </div>
-          <div class="mx-2">
-              
-              <p class="card-text p-y-1">Some quick example text to build on the card title .</p>
-          </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="card pb-5">
-            <div class="card-block">
-            <div class="card-title bg-success px-2">
-          User
-          </div>
-          <div class="mx-2">
-              <li class="card-text my-1">User</li>
-              <li class="card-text my-1">Profile</li>
-          </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="card pb-5">
-            <div class="card-block">
-            <div class="card-title bg-success px-2">
-        System
-          </div>
-          <div class="mx-2">
-              <li class="card-text my-1">Back Up Database</li>
-              <li class="card-text my-1">Upload Database</li>
-
-          </div>
-            </div>
-          </div>
-        </div>
-
-
+    <div class="row">
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-title bg-success text-white px-4">
+        Location
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="province.php" class="card-link">Province</a>
+          </li>
+          <li class="list-group-item">
+            <a href="municipality.php" class="card-link">Municipality</a>
+          </li>
+          <li class="list-group-item">
+            <a href="barangay.php" class="card-link">Barangay</a>
+          </li>
+        </ul>
       </div>
     </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-title bg-success text-white px-4">
+        Producers
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="" class="card-link">Some quick example text t</a>
+          </li>
+          <li class="list-group-item">
+            <a href="" class="card-link">Some quick example text t</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-title bg-success text-white px-4">
+        Production
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="" class="card-link">Some quick example text t</a>
+          </li>
+          <li class="list-group-item">
+            <a href="" class="card-link">Some quick example text t</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-title bg-success text-white px-4">
+        News
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="" class="card-link">Some quick example text to bui.</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-title bg-success text-white px-4">
+        User
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="system_user.php" class="card-link">User</a>
+          </li>
+          <li class="list-group-item">
+            <a href="edit_systemuser.php" class="card-link">Profile</a>
+          </li>
+          <li class="list-group-item">
+            <a href="user_type.php" class="card-link">User Type</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-title bg-success text-white px-4">
+        System
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="backup_button.php" class="card-link">Back Up Database</a>
+          </li>
+          <li class="list-group-item">
+            <a href="" class="card-link">Upload Database</a>
+          </li>
+          <li class="list-group-item">
+            <a href="audit_trail.php" class="card-link">Audit Trail</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
   </div>
 
   </main>
