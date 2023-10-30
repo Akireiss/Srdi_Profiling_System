1903e41cb3bc85e1d5197c2c818c6ec57da6aa4a
Akireiss <joshcstnd@gmail.com>
2023-10-30 15:46:34 +0800

    dsadadds

---
+++ includes/testside.php
@@ -0,0 +1,203 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link " href="dashboard.php">
+      <i class="bi bi-grid" style="color: white;"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard  Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed"  href="ct.php">
+
+      <i class="bi bi-people" style="color: white;"></i><span>Cocoon Producers</span></i>
+    </a>
+  </li>
+
+<!--Project Site-->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="projectsite.php">
+      <i class="bi bi-house" style="color: white;"></i>
+      <span>Project Site</span>
+    </a>
+    
+  </li><!-- Project Site Nav -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="productions.php">
+    <i class="bi bi-tools" style="color: white;"></i>
+      <span>Production</span>
+    </a>
+    
+  </li><!-- Project Site Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#system-settings" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-gear" style="color: white;"></i><span>System Settings</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="system-settings" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+
+      <p class="text-white mx-3 ml-3 mt-2 fw-bold">Location</p>
+
+      <li>
+          <a href="region.php">
+            <i class="bi bi-circle" ></i><span>Region</span>
+          </a>
+        </li>
+        <li>
+          <a href="province.php">
+            <i class="bi bi-circle" ></i><span>Province</span>
+          </a>
+        </li>
+        <li>
+          <a href="municipality.php">
+            <i class="bi bi-circle" ></i><span>Municipality</span>
+          </a>
+        </li>
+        <li>
+          <a href="barangay.php">
+            <i class="bi bi-circle" ></i><span>Barangay</span>
+          </a>
+        </li>
+
+
+
+        <p class="text-white mx-3 ml-3 mt-2 fw-bold">Components</p>
+      <li>
+          <a href="agency.php">
+            <i class="bi bi-circle" ></i><span>Funding Agency</span>
+          </a>
+        </li>
+        <li>
+          <a href="civil_status.php">
+            <i class="bi bi-circle" ></i><span>Civil Status</span>
+          </a>
+        </li>
+        <li>
+          <a href="religion.php">
+            <i class="bi bi-circle" ></i><span>Religion</span>
+          </a>
+        </li>
+        <li>
+          <a href="education.php">
+            <i class="bi bi-circle" ></i><span>Education</span>
+          </a>
+        </li>
+        <li>
+          <a href="topography.php">
+            <i class="bi bi-circle" ></i><span>Topography</span>
+          </a>
+        </li>
+        <li>
+          <a href="source_of_income.php">
+            <i class="bi bi-circle" ></i><span>Source of Income</span>
+          </a>
+        </li>
+        <li>
+          <a href="farm_tools.php">
+            <i class="bi bi-circle" ></i><span>Farm Tools</span>
+          </a>
+        </li>
+        <li>
+          <a href="land_type.php">
+            <i class="bi bi-circle" ></i><span>Land Type</span>
+          </a>
+        </li>
+        <li>
+          <a href="tenancy.php">
+            <i class="bi bi-circle" ></i><span>Tenancy Status</span>
+          </a>
+        </li>
+        <li>
+          <a href="irrigation.php">
+            <i class="bi bi-circle" ></i><span>Souce of Irrigation</span>
+          </a>
+        </li>
+        <li>
+          <a href="soil_type.php">
+            <i class="bi bi-circle" ></i><span>Soil Type</span>
+          </a>
+        </li>
+       
+        <li>
+          <a href="year.php">
+            <i class="bi bi-circle" ></i><span>Year</span>
+          </a>
+        </li>
+      
+      
+</ul> 
+  </li>
+  <!-- End Settings Nav -->
+  <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-person-workspace" style="color: white;"></i><span>System</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+          <a href="backup_button.php">
+            <i class="bi bi-circle"></i><span>Back up Database</span>
+          </a>
+        </li>
+       
+        <li>
+          <a href="monitoring_team.php">
+            <i class="bi bi-circle" ></i><span>Upload Database</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="audit_trail.php">
+            <i class="bi bi-circle"></i><span>Audit Trail</span>
+          </a>
+        </li>
+        
+</ul>
+   
+  </li><!-- End Settings Nav -->
+
+
+    <!-- End Settings Nav -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#charts-side" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-person-workspace" style="color: white;"></i><span>User Settings</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="charts-side" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+          <a href="system_user.php">
+            <i class="bi bi-circle" ></i><span>System</span>
+          </a>
+        </li>
+        <li>
+          <a href="user_type.php">
+            <i class="bi bi-circle" ></i><span>User Type</span>
+          </a>
+        </li>
+        <li>
+          <a href="monitoring_team.php">
+            <i class="bi bi-circle" ></i><span>Monitoring Team</span>
+          </a>
+        </li>
+        
+</ul>
+   
+  </li>
+
+
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="report.php">
+      <i class="bx bxs-report" style="color: white;"></i><span>Reports</span>
+    </a>
+    
+  
+</li><!-- End Reports Nav -->
+
+<!-- End Blank Page Nav -->
+
+</ul>
+
+</aside><!-- End Sidebar-->
